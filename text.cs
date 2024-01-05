287e4f9c6c72407959ef66a680572c2eecae7481
Anastasiia Khovanska <98605728+akhovanska@users.noreply.github.com>
2024-01-05 23:47:45 +0200

    Update text.cs

--- text.cs
+++ text.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Import the TextMeshPro namespace
 
 public class HelloWorld : MonoBehaviour
 {
     public string firstName;
     public string lastName;
-    private TextMeshProUGUI TextMeshPro
+    private TextMeshProUGUI textMeshPro; // Correct the variable name
 
     // Start is called before the first frame update
     void Start()
     {
-        TextMeshPro = GetComponent<TextMeshProUGUI>();
-        TextMeshPro.text = $"Hello {firstName} {lastName}!";
+        textMeshPro = GetComponent<TextMeshProUGUI>(); // Correct the variable name
+        textMeshPro.text = $"Hello {firstName} {lastName}!";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // No need for anything in Update for this example
     }
 }
